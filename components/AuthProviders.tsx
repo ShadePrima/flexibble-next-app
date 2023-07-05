b308274b417e9db3e1acd4f45905fad190ca1ed8
ShadePrima <aleksandrag1984@gmail.com>
2023-07-05 22:18:34 +0300

    fix env

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,29 @@
+'use client'
 import React from 'react'
+import { getProviders, signIn } from 'next-auth/react'
+
+type Provider = {
+  id: string
+  name: string
+  type: string
+  signinUrl: string
+  callbackUrl: string
+  signinUrlParams: Record<string, string> | null
+}
+
+type Providers = Record<string, Provider>
 
 const AuthProviders = () => {
-  return <div>AuthProviders</div>
+  const [providers, setProviders] = React.useState<Providers | null>(null)
+
+
+  if(providers) {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
 
 export default AuthProviders
